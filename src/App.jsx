d947584e289d43e74d44408683e300a3a630cf3d
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-09-25 12:57:45 +0100

    added layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
-
+import {
+  About,
+  AuthWrapper,
+  Cart,
+  Products,
+  SingleProduct,
+  Error,
+  Home,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,21 @@
+import About from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import Cart from "./CartPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Error from "./ErrorPage";
+import Home from "./HomePage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  About,
+  AuthWrapper,
+  Cart,
+  Products,
+  SingleProduct,
+  Error,
+  Home,
+  Checkout,
+  PrivateRoute,
+};
