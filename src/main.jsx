cc2db95bead9a66493d2025f2fe3a04955712cd8
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-10-18 13:42:59 +0100

    starting user auth

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
+//dev-oqpeylui7jliglrx.us.auth0.com
+// TvBptpbUCQQjP4814xP974pCiGUXduJp
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
